94ccdacc736a07d0a75a779ab946563627f742bd
Rafaela <2erica.leonelo@gmail.com>
2024-09-25 21:06:38 -0300

    função que verifica se uma frase é um palíndromo adicionado;  Função que retorna valores máx e min encontrados dentro do array adicionado.

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
+function isPalindrome(frase){
+    
+        const cleanedStr = frase.replace(/[\W_]/g, '').toLowerCase();
+        const length = cleanedStr.length;
+    
+        
+        for (let i = 0; i < length / 2; i++) {
+            
+            if (cleanedStr[i] !== cleanedStr[length - 1 - i]) {
+                //return false; 
+            }
+        }
+        return true; 
+    }
+    
+    console.log(isPalindrome("Ame o Poema"));
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+
+
+
+    function arrayMaxMin(arr) {
+        
+        let min = arr[0];
+        let max = arr[0];
+    
+         for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i]; 
+            }
+            if (arr[i] > max) {
+                max = arr[i]; 
+            }
+        }
+    
+         return [min, max];
+    }
+
+    let array = [4,2,6,3,9]
+    let result = arrayMaxMin(array)
+    console.log(result);
+    
+    
+
+
+
